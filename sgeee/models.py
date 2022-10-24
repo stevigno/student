1076576d5db29a54163de5cd89b7d6a37b6d2c26
ndjouonkep steve <58404283+stevigno@users.noreply.github.com>
2022-10-24 17:19:53 +0300

    Update models.py

--- sgeee/models.py
+++ sgeee/models.py
@@ -11,4 +11,12 @@ class niveau(models.Model):
 
 class question(models.Model):
     id_critere = models.IntegerField(null = False)
-    intitule = models.CharField(max_length = 100)    
+    intitule = models.CharField(max_length = 100)
+
+class suggestion(models.Model):
+    content_suggestion = models.TextField()
+    id_etudiant = models.IntegerField(max_length = 11)
+    id_session_evaluation = models.IntegerField(max_length = 11)
+    id_ue = models.IntegerField(max_length = 11)
+    date_soumission = models.DateTimeField()
+
